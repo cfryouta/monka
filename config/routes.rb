56ad5ce6622a05d274be5c3849e367f0304b2781
k_ryouta <k_ryouta@centerfield.co.jp>
2017-01-20 05:44:05 +0000

    画面遷移とルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :taggings
-  resources :tags
-  resources :books
+  resources :tags, except: :show
+  resources :books do
+    resources :taggings, only: [:new, :edit, :create, :update, :destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
