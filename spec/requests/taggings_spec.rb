5fd31fb1b74db81cb09bd4f7c6307bb8704c0a12
k_ryouta <k_ryouta@centerfield.co.jp>
2017-01-23 08:22:02 +0000

    リクエストテスト

--- spec/requests/taggings_spec.rb
+++ spec/requests/taggings_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Taggings", type: :request do
-  describe "GET /taggings" do
-    it "works! (now write some real specs)" do
-      get taggings_path
-      expect(response).to have_http_status(200)
-    end
-  end
-end
+# RSpec.describe "Taggings", type: :request do
+#   describe "GET /taggings" do
+#     it "works! (now write some real specs)" do
+#       get taggings_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
