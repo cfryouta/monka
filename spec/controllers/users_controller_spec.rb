93c1be922c2c17fafeb1b82e6872279bbd84ae6c
k_ryouta <k_ryouta@centerfield.co.jp>
2017-01-23 08:12:18 +0000

    ルーティングテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,6 +34,8 @@ RSpec.describe UsersController, type: :controller do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # UsersController. Be sure to keep this updated too.
+  let(:admin_user) { User.all.first }
+  before(:each) {sign_in admin_user }
   let(:valid_session) { {} }
 
   describe "GET #index" do
--- spec/routing/taggings_routing_spec.rb
+++ spec/routing/taggings_routing_spec.rb
@@ -2,37 +2,32 @@ require "rails_helper"
 
 RSpec.describe TaggingsController, type: :routing do
   describe "routing" do
+    let(:book) { Book.first }
+    let(:tagging) { book.taggings.first }
 
-    it "routes to #index" do
-      expect(:get => "/taggings").to route_to("taggings#index")
-    end
 
     it "routes to #new" do
-      expect(:get => "/taggings/new").to route_to("taggings#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/taggings/1").to route_to("taggings#show", :id => "1")
+      expect(:get => "/books/1/taggings/new").to route_to("taggings#new", :book_id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/taggings/1/edit").to route_to("taggings#edit", :id => "1")
+      expect(:get => "/books/1/taggings/1/edit").to route_to("taggings#edit", :id => "1", :book_id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/taggings").to route_to("taggings#create")
+      expect(:post => "/books/1/taggings").to route_to("taggings#create", :book_id => "1")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/taggings/1").to route_to("taggings#update", :id => "1")
+      expect(:put => "/books/1/taggings/1").to route_to("taggings#update", :id => "1", :book_id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/taggings/1").to route_to("taggings#update", :id => "1")
+      expect(:patch => "/books/1/taggings/1").to route_to("taggings#update", :id => "1", :book_id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/taggings/1").to route_to("taggings#destroy", :id => "1")
+      expect(:delete => "/books/1/taggings/1").to route_to("taggings#destroy", :id => "1", :book_id => "1")
     end
 
   end
