ed98f29a964b92511cf5f468d0cdb1035e5d95d1
k_ryouta <k_ryouta@centerfield.co.jp>
2017-01-25 09:06:22 +0000

    2017/01/25 3rd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smpt_settings = {
+  config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
     address: 'smtp.gmail.com',
     port: '587',
