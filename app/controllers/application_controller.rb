e716e061c0a5a310d6f193a8217aeb0d2afb521a
k_ryouta <k_ryouta@centerfield.co.jp>
2017-01-26 04:40:54 +0000

    カートのセッション管理機能の準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def after_sign_in_path_for(resouce)
-    books_path
+    if current_user.role == 'admin'
+      books_path
+    else
+      root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   resources :orders, only: [:new, :create]
   resources :musics
   resources :orders_management, only: [:index, :edit] do
